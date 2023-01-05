485f154b3e50669c3c770b1f3fadd0a2132554e1
Mike Smith <mikesmithlab@gmail.com>
2023-01-05 16:21:19 +0000

    added pydates import external pkg

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("LICENSE", "r") as fh:
-    license = fh.read()
-
 setuptools.setup(
-    name='dates',
+    name='emails',
     version='0.1',
-    license=license,
-    packages=setuptools.find_packages(
-        exclude=('tests', 'docs')
-    ),
-    url='https://github.com/MikeSmithLabTeam/dates',
-    install_requires=[
-        'datetime',
-        'pywin32'
-    ],
+    author='Mike Smith',
+    author_email='mikesmithlab@gmail.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mikesmithlab/pydates',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=['pywin32','pydates', 'datetime'],
 )
